e7ed00c8ba214705a3512c056013863013d846d9
cristao.sidney <cristao.sidney@gmail.com>
2025-03-07 19:42:55 -0300

    Menu inicial

---
+++ src/components/GrupoConta.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GrupoConta = () => {
+  return <h1>GrupoConta CRUD</h1>;
+};
+
+export default GrupoConta;
---
+++ src/components/SubConta.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubConta = () => {
+  return <h1>SubConta CRUD</h1>;
+};
+
+export default SubConta;
