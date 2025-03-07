0acbe3f1a2befdfa53dceb8b5a945cd5573f9e86
cristao.sidney <cristao.sidney@gmail.com>
2025-03-07 19:56:31 -0300

    Menu inicial 3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
+import AuthProvider from "./components/AuthProvider";
+import { CssBaseline } from "@mui/material";
 
-const domain = "dev-e6tkv2b8tbmr4k5m.us.auth0.com";
-const clientId = "AlnVyav7HxNWT3Zgti3TG9aD81itMJCS";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-    audience={"https://my-app-financy-backend.onrender.com/api"}
-    scope={"openid profile email"}    
-  >
-    <Router>
-      <App />
-    </Router>
-  </Auth0Provider>,
-  document.getElementById('root')
+import './index.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <AuthProvider>
+    <CssBaseline />
+    <App />
+  </AuthProvider>,
 );
